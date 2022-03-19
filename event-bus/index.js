69d06d8337fb060e79cd3c11d6dfe642e11daa43
ian <ianbalijawa16@gmail.com>
2022-03-19 09:43:04 +0300

    modification: update the docker ignore file

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const axios = require("axios");
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 const events = [];
 
